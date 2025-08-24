8791356d72ea09a3c6d1fc675537c21ab0e5bf8a
Keplin1 <irin.keplin@gmail.com>
2025-08-24 19:17:34 +0100

    added images to product page

--- junior-dev-tech-test-main/src/components/Product.vue
+++ junior-dev-tech-test-main/src/components/Product.vue
@@ -1,7 +1,18 @@
 <template>
   <div id="product">
     <!-- Add your code here -->
-    <p class="text-center">{{ product.product_title }}</p>
+
+    <div id="parent">
+      <div id="left-container">
+        <img v-for="(image_data, index) in product.media_gallery" :key="index" class="image-container "
+          :src="image_data.image" :alt="image_data.alt" />
+      </div>
+      <div id="right-container">
+        <p class="text-center">{{ product.product_title }}</p>
+      </div>
+    </div>
+
+
   </div>
 </template>
 
@@ -21,4 +32,35 @@ export default {
 </script>
 
 <style scoped lang="scss">
-// Styling to be added here if needed. SASS is allowed if preferred</style>
+#parent {
+  display: flex;
+  margin-left: 2.5%;
+  margin-right: 2.5%;
+}
+
+#left-container {
+  display: flex;
+  flex-basis: 60%; // Fixed 60% width
+  flex-flow: row wrap;
+
+}
+
+#right-container {
+  display: flex;
+  flex-basis: 40%;
+  flex-direction: column;
+}
+
+.image-container {
+  // flex-basis: 50%; // Each image takes 50% of container width minus padding, making them appear side-by-side
+  padding: 5px; // between each image
+
+}
+
+img {
+  max-width: 50%;
+  max-height: 50%; // Fill the container width
+
+}
+</style>
+<!-- // Styling to be added here if needed. SASS is allowed if preferred</style> -->
